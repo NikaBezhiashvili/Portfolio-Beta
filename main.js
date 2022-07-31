842c2bfc90d901c14d29639a5f624f0a0eea36ba
NikaBezhiashvili <74880697+NikaBezhiashvili@users.noreply.github.com>
2022-07-31 13:12:14 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let button = document.getElementById('btn')
 let main_text = document.getElementById('main_text')
-
+let stars = document.getElementById('backstars')
 window.addEventListener('scroll', function(){
     value = window.scrollY;
     button.style.marginTop = value + 'px';
     main_text.style.paddingTop = value + 'px';
+    stars.style.top = value + 'px';
+    stars.style.left = value * 0.1 + 'px';
 
 })
